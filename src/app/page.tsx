5b103bda6741e2e14987411054abe31b553d5b2f
Alexander Royal <alexander.de.royal@gmail.com>
2023-10-27 09:09:43 -0400

    new directory created

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import MaxWidthWrapper from '@/components/MaxWidthWrapper'
+
+export default function Home() {
+  return (
+      <MaxWidthWrapper className='mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center'>
+        <div className='mx-auto mb-4 flex mx-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50'>
+        <p className='text-sm font-semibold text-gray-700'>Quill is now public</p>
+        </div>
+          <h1 className='max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl'>
+              Chat with your <span className={'text-blue-600'}>documents</span> in seconds.
+          </h1>
+          <p className={'mt-5 max-w-prose text-zinc-700 sm:text-lg'}>
+              Quill allows you to have conversations with any PDF documents. Simply upload your file and start asking questions right away.
+          </p>
+      </MaxWidthWrapper>
+
+  )
+}
