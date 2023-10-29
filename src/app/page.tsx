cf5cb84b5c151000bf423831c5f20473b5f74fad
Alexander Royal <alexander.de.royal@gmail.com>
2023-10-29 05:34:58 -0400

    completed starter files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image'
 import MaxWidthWrapper from '@/components/MaxWidthWrapper'
+import Link from "next/link";
+import {ArrowRight} from "lucide-react"
+import {buttonVariants} from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -13,7 +16,24 @@ export default function Home() {
           <p className={'mt-5 max-w-prose text-zinc-700 sm:text-lg'}>
               Quill allows you to have conversations with any PDF documents. Simply upload your file and start asking questions right away.
           </p>
+
+          <Link className={buttonVariants({
+              size: 'lg',
+              className: "mt-5"
+          })}
+                href='/dashboard'
+                target='_blank'>
+              Get started <ArrowRight className='ml-2 h-5 w-5'/>
+          </Link>
       </MaxWidthWrapper>
+
+    <div>
+        <div className='relative isolate'>
+            <div aria-hidden="true" className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
+                <div className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-background-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w[72.1875rem]'></div>
+            </div>
+        </div>
+    </div>
 
   )
 }
